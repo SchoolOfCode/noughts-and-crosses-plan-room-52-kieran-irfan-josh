c4fd6a05ea4475a38312d045e9888f7e47d17fca
KieranSOC <kieranSOC@Kierans-MacBook-Air.local>
2022-02-09 23:14:44 +1100

    Game component started

--- noughts_and_crosses/src/Components/Game/Game.js
+++ noughts_and_crosses/src/Components/Game/Game.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Game() {
+  let board = [null, null, null, null, null, null, null, null, null];
+  const [board, setBoard] = useState(board);
+
+  const [xturn, setXturn] = useState();
+
+  function makeMove(index) {
+    if (board[index] === null) {
+      setBoard;
+    }
+  }
+
   return <div>Game</div>;
 }
 
